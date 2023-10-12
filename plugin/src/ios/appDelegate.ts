25ab99a17d1c9244f8f64ede4040644eea706bd4
Baptiste Laget <blaget@cloudraker.com>
2023-10-12 17:18:37 -0400

    fix: fix AppDelegate.mm replace function

--- plugin/src/ios/appDelegate.ts
+++ plugin/src/ios/appDelegate.ts
@@ -27,7 +27,7 @@ export function modifyObjcAppDelegate(contents: string): string {
       "return [super application:application didFinishLaunchingWithOptions:launchOptions];",
       `${methodInvocationBlock}
       
-return [super application:application didFinishLaunchingWithOptions:launchOptions]`
+return [super application:application didFinishLaunchingWithOptions:launchOptions];`
     );
   }
 
